ba139aecaa50f10cb1dbadf91a35ab78af7a6e06
Igor <igorfx@mail.ru>
2025-04-18 13:44:21 +0600

    fixes README.md

--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,6 @@
 def get_mask_card_number(card_number: int) -> str:
-    """Функция принимает номер карты в str и возвращает в виде XXXX XX** **** XXXX"""
+    """Функция принимает номер карты в формате int и возвращает в формате str
+    виде XXXX XX** **** XXXX"""
     str_card_number = str(card_number)
     masked_card_number = (
         f"{str_card_number[:4]} {str_card_number[4:6]}** **** {str_card_number[12:]}"
@@ -8,7 +9,7 @@ def get_mask_card_number(card_number: int) -> str:
 
 
 def get_mask_account(account_number: int) -> str:
-    """Функция принимает номер счета в int и возвращает в виде **XXXX"""
+    """Функция принимает номер счета в int и возвращает в формате str в виде **XXXX"""
     str_account_number = str(account_number)
     masked_account_number = f"**{str_account_number[-4:]}"
     return masked_account_number
