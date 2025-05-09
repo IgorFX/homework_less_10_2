92d8f7d785ff89e50d149fe270013a7854f858b9
Igor <igorfx@mail.ru>
2025-05-09 14:56:47 +0600

    fix conftest

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,25 +1,19 @@
+import pytest
+
 from src.widget import get_date, mask_account_card
 
 
-def test_mask_account_card_card():
-    assert (
-        mask_account_card("Visa Platinum 8990922113665229")
-        == "Visa Platinum 8990 92** **** 5229"
-    )
-
-
-def test_empty_mask_account_card_card():
-    assert mask_account_card("") == "Не указан номер карты"
-
-
-def test_name_card_card():
-    assert (
-        mask_account_card("1234567887654321") == "Отсутствует наименование карты(счета)"
-    )
-
-
-def test_mask_account_card_account():
-    assert mask_account_card("Счет 45878990922113665229") == "Счет **5229"
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
+        ("1234567887654321", "Отсутствует наименование карты(счета)"),
+        ("Счет 45878990922113665229", "Счет **5229"),
+        ("", "Данные отсутствуют"),
+    ],
+)
+def test_mask_account_card(value, expected):
+    assert mask_account_card(value) == expected
 
 
 def test_get_date():
